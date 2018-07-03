b4a3642913a5798397dbb7b8635d538743667a32
Okorokov <awwdrrg@rambler.ru>
2018-07-03 08:13:13 +0300

    изменил index.json

--- src/main/java/com/example/hpsus/kotlintest/InfoActivity.kt
+++ src/main/java/com/example/hpsus/kotlintest/InfoActivity.kt
@@ -22,21 +22,21 @@ import java.util.*
 
 class InfoActivity : AppCompatActivity() {
     val Tag: String = "InfoActivity"
-    var mHomeIA:mHome=mHome()
-    var prHomeIA:mHome=mHome()
+    var mHomeIA: mHome = mHome()
+    var prHomeIA: mHome = mHome()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
-        var intent= intent
+        var intent = intent
         //Log.d(Tag, "KEY_INFO_NAMBERPUT- " + intent.getStringExtra(DBHelper.KEY_INFO_NAMBERPUT))
         supportActionBar!!.setTitle("Дом " + intent.getStringExtra(HOME_COL_NAMEHOME))
 
 
-        mHomeIA.indiHome=intent.getIntExtra(HOME_COL_INDIHOME,0)
-        mHomeIA.nameHome=intent.getStringExtra(HOME_COL_NAMEHOME)
-        mHomeIA.floors=intent.getIntExtra(HOME_COL_FLOORS,0)
-        mHomeIA.developerName=intent.getStringExtra(HOME_COL_DEVELOPERNAME)
+        mHomeIA.indiHome = intent.getIntExtra(HOME_COL_INDIHOME, 0)
+        mHomeIA.nameHome = intent.getStringExtra(HOME_COL_NAMEHOME)
+        mHomeIA.floors = intent.getIntExtra(HOME_COL_FLOORS, 0)
+        mHomeIA.developerName = intent.getStringExtra(HOME_COL_DEVELOPERNAME)
     }
 
     override fun onBackPressed() {
@@ -57,37 +57,39 @@ class InfoActivity : AppCompatActivity() {
         }
         return super.onOptionsItemSelected(item)
     }
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
+
     fun showDialog() {
-val li = LayoutInflater.from(this)
-val promptsView = li.inflate(R.layout.dialog_info, null)
-val mDialogBuilder = AlertDialog.Builder(this)
-mDialogBuilder.setView(promptsView)
-        var tvindiHomeText=promptsView.findViewById(R.id.tvindiHomeText) as EditText
-        val tvnameHomeText=promptsView.findViewById(R.id.tvnameHomeText) as EditText
-        val tvfloorsText=promptsView.findViewById(R.id.tvfloorsText) as EditText
-        val tvdeveloperText=promptsView.findViewById(R.id.tvdeveloperText) as EditText
+        val li = LayoutInflater.from(this)
+        val promptsView = li.inflate(R.layout.dialog_info, null)
+        val mDialogBuilder = AlertDialog.Builder(this)
+        mDialogBuilder.setView(promptsView)
+        var tvindiHomeText = promptsView.findViewById(R.id.tvindiHomeText) as EditText
+        val tvnameHomeText = promptsView.findViewById(R.id.tvnameHomeText) as EditText
+        val tvfloorsText = promptsView.findViewById(R.id.tvfloorsText) as EditText
+        val tvdeveloperText = promptsView.findViewById(R.id.tvdeveloperText) as EditText
 
         tvindiHomeText.setText(mHomeIA.indiHome)
         tvnameHomeText.setText(mHomeIA.nameHome)
         tvfloorsText.setText(mHomeIA.floors)
         tvdeveloperText.setText(mHomeIA.developerName)
 
-mDialogBuilder
-.setCancelable(false)
-        .setPositiveButton("ОК"){dialog, which ->}
+        mDialogBuilder
+                .setCancelable(false)
+                .setPositiveButton("ОК") { dialog, which -> }
 
 
- //Создаем AlertDialog:
+        //Создаем AlertDialog:
         val alertDialog = mDialogBuilder.create()
 
- //и отображаем его:
+        //и отображаем его:
         alertDialog.show()
 
-}
+    }
 }
 
 
